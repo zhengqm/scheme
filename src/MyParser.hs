6f1d3e677075dd2e34c477b1d5e7b6a2aae46acb
Qimu Zheng <zheng.qm@163.com>
2017-03-18 21:19:59 +0800

    parser finish

--- src/MyParser.hs
+++ src/MyParser.hs
@@ -55,5 +55,43 @@ parseAtom = do
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
 
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
+parseListCombined :: Parser LispVal
+parseListCombined = do
+    char '('
+    x <- try parseList <|> parseDottedList
+    char ')'
+    return x
+
 parseExpr :: Parser LispVal
-parseExpr = parseAtom <|> parseString <|> parseNumber
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+         <|> parseQuoted
+         <|> parseListCombined
+
+
+
+
+
+
+
+
+
+
+
+
