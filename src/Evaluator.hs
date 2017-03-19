96dd6d0544fb4a9f15f3a3719db0c047117839e4
Qimu Zheng <zheng.qm@163.com>
2017-03-19 10:41:41 +0800

    basic primitives added

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -7,4 +7,54 @@ eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
 
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = case lookup func primitives of 
+                    Just f  -> f args
+                    Nothing -> Bool False
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [
+    ("+", numericBinop (+)),
+    ("-", numericBinop (-)),
+    ("*", numericBinop (*)),
+    ("/", numericBinop div),
+    ("mod", numericBinop mod),
+    ("quotient", numericBinop quot),
+    ("remainder", numericBinop rem),
+    ("symbol?", unaryOp symbolp),
+    ("string?", unaryOp stringp),
+    ("number?", unaryOp numberp),
+    ("bool?", unaryOp boolp),
+    ("list?", unaryOp listp)]
+
+numericBinop :: (Integer->Integer->Integer)->[LispVal]->LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unaryOp :: (LispVal -> LispVal) -> [LispVal] -> LispVal
+unaryOp f [v] = f v
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
+                            if null parsed
+                                then 0
+                                else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
+
+symbolp, numberp, stringp, boolp, listp :: LispVal -> LispVal
+symbolp (Atom _)   = Bool True
+symbolp _          = Bool False
+numberp (Number _) = Bool True
+numberp _          = Bool False
+stringp (String _) = Bool True
+stringp _          = Bool False
+boolp   (Bool _)   = Bool True
+boolp   _          = Bool False
+listp   (List _)   = Bool True
+listp   (DottedList _ _) = Bool True
+listp   _          = Bool False
